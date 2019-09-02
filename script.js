7df992814d7800de1e357f0513407cd3d1ae2244
Daniel Nordstrom <d@mrnordstrom.com>
2019-09-02 23:14:47 +0200

    Refactored to classes. For the kicks.

--- script.js
+++ script.js
@@ -9,65 +9,7 @@
     { id: 6, name: 'Tesla', location: 'Palo Alto' }
   ]
 
-  const listElement = document.getElementById('list')
-
-  // Iterate over companies, appending an LI element to the list for each
-  companies.forEach(company => {
-    const listItem = document.createElement('li')
-
-    // Put these here so we don't have to lookup the company array on click
-    listItem.setAttribute('data-company-id', company.id)
-    listItem.setAttribute('data-company-name', company.name)
-    listItem.setAttribute('data-company-location', company.location)
-
-    // Text displayed on page
-    listItem.textContent = `${company.name} (${company.location})`
-
-    // Add item to the page
-    listElement.appendChild(listItem)
-  })
-
-  // Add click event listener to list element. Clicks on individual child items
-  // will bubble up to this parent--no need for multiple listeners.
-  listElement.addEventListener('click', ({ target }) => {
-    // Check if clicked element is a list item with a company name. Adding a CSS
-    // class to the element and using `listItem.classList.contains()` to check
-    // for it is probably faster than matching against a CSS selector, but has
-    // worse browser compatibility (and checking `className` itself is a bit
-    // ugly if we add multiple class names).
-    if (target.matches('li') &&
-        target.hasAttribute('data-company-name') &&
-        target.hasAttribute('data-company-location')) {
-
-      const name = target.getAttribute('data-company-name')
-      const location = target.getAttribute('data-company-location')
-      
-      notify(`${name} is located in ${location}`)
-    }
-  })
-
-  // Request notification permission if browser supports it
-  if ('Notification' in window) {
-    Notification.requestPermission()
-  }
-
-  /**
-   * Display notification message in system tray. If notifications are not
-   * supported (e.g. older browser or browsing locally with `file://`) or we
-   * don't have appropriate permissions, use `console.log`.
-   * 
-   * `npx serve` or similar can be used to serve the HTML locally with support
-   * for Notification API.
-   * 
-   * @param message Message to display
-   */
-  function notify(message) {
-    // Check if notifications are supported and we were granted permission
-    if (('Notification' in window) && Notification.permission === 'granted') {
-      new Notification(message)
-    } else {
-      console.log(`NOTIFICATION: ${message}`)
-    }
-  }
+  // Create CompanyList instance for `#list` element using the API data
+  CompanyList.from(companies, { selector: '#list'})
 
 })()
