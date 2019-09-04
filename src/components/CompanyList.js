8a3b0bd26f080a963552fc7a64e4993b2c31e200
Daniel Nordstrom <d@mrnordstrom.com>
2019-09-04 21:52:54 +0200

    Single quotes are prettier than double quotes.

--- src/components/CompanyList.js
+++ src/components/CompanyList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import CompanyListItem from './CompanyListItem';
 import FilterButton from './FilterButton';
 import Actions from '../actions';
